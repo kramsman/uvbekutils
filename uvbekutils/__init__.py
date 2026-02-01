a8337825ced38eac5950a7e9932a0506716b5512
brian kramer <kramsman@yahoo.com>
2026-02-01 15:08:29 -0500

    copied init.py from bekutils to make functions available

--- uvbekutils/__init__.py
+++ uvbekutils/__init__.py
@@ -31,4 +31,4 @@ from .bek_funcs import conc_addr_desc
 from .bek_funcs import conc_addr_remove_desc
 from .sumby_w_totals import sumby_w_totals
 from .select_from_list import select_from_list
-from .test_foreign_git_edit import test_foreign_git_edit
+# from .test_foreign_git_edit import test_foreign_git_edit
